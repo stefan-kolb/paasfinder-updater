29a3638faf4d4369741f7bc050bb01f1011ae127
Petr Vasilyev <petr.vasilyev@stud.uni-bamberg.de>
2017-03-16 14:41:10 +0100

    charset

--- worker/src/main/java/BaseEncoding.java
+++ worker/src/main/java/BaseEncoding.java
@@ -1,20 +1,15 @@
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class BaseEncoding {
-    private static final String CHARSET = "utf-8";
     public static void main( String[] args) {
-        try {
-            // Encode using basic encoder
-            String encodedString = Base64.getEncoder().encodeToString("Secret".getBytes(CHARSET));
-            System.out.println("Encoded String: " + encodedString);
+        // Encode using basic encoder
+        String encodedString = Base64.getEncoder().encodeToString("Secret".getBytes(StandardCharsets.UTF_8));
+        System.out.println("Encoded String: " + encodedString);
 
-            // Decode
-            byte[] decodedString = Base64.getDecoder().decode(encodedString);
-            System.out.println("Decoded String: " + new String(decodedString, CHARSET));
-        } catch(UnsupportedEncodingException e){
-            System.out.println("Error :" + e.getMessage());
-        }
+        // Decode
+        byte[] decodedString = Base64.getDecoder().decode(encodedString);
+        System.out.println("Decoded String: " + new String(decodedString, StandardCharsets.UTF_8));
     }
 
 }
