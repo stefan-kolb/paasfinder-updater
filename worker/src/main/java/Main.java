73d45f6eeac3155eb1f23b77572b1ca30ab6c26a
Petr Vasilyev <petr.vasilyev@stud.uni-bamberg.de>
2017-04-29 19:02:09 +0200

    added apache id generator

--- worker/src/main/java/Main.java
+++ worker/src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         UpdateClient client = new UpdateClient();
-        final String branchName = "testing";
+        final String branchName = "updating-" + "bla" + "-" + UpdateService.generateUniqueId();
 
         client.createBranch(branchName);
         client.updateFile("acquia_cloud", "Neue Zeile", "update acquia_cloud", branchName);
--- worker/src/main/java/UpdateService.java
+++ worker/src/main/java/UpdateService.java
@@ -1,59 +1,20 @@
-import java.util.*;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonSyntaxException;
+import org.apache.commons.lang.RandomStringUtils;
 import org.json.JSONObject;
-import vendor.*;
 import static spark.Spark.*;
 
 public class UpdateService {
 
-    public static class Model {
-        private static List<Vendor> vendors = new ArrayList<>();
+    public static final int ID_LENGTH = 10;
 
-        public static Vendor store(Vendor vendor){
-            vendors.add(vendor);
-            return vendor;
-        }
-
-        public List<Vendor> getAllVendors(){
-            return vendors;
-        }
+    public static String generateUniqueId() {
+        return RandomStringUtils.randomAlphanumeric(ID_LENGTH);
     }
-
-    private static Gson GSON = new GsonBuilder().create();
 
     public static void main( String[] args) {
         port(9090);
 
-        Model model = new Model();
-
-        redirect.get("/", "/vendors");
-
         before((request,response)-> {
             response.header("Access-Control-Allow-Origin", "*");
-        });
-
-        get("/vendors", (request, response) -> {
-            response.status(200);
-            response.type("application/json; charset=utf-8");
-            return GSON.toJson(model.getAllVendors());
-        });
-
-        post("/vendors", "application/json", (request, response) -> {
-            JSONObject json = new JSONObject(request.body());
-
-            final String fileName =  json.get("id").toString().toLowerCase();
-            final String branchName = "updating-" + fileName;
-
-            UpdateClient client = new UpdateClient();
-            client.createBranch(branchName);
-            client.updateFile(fileName, json.toString(), "update message", branchName);
-            client.createPullRequest("title", "message", branchName);
-
-            response.status(200);
-            return "OK";
         });
 
         options("/*", (request,response)-> {
@@ -68,5 +29,21 @@ public class UpdateService {
             }
             return "OK";
         });
+
+        post("/vendor", "application/json", (request, response) -> {
+            JSONObject json = new JSONObject(request.body());
+
+            final String fileName =  json.get("vendorAPIKey").toString();
+            final String branchName = "updating-" + fileName + "-" + generateUniqueId();
+
+            UpdateClient client = new UpdateClient();
+            client.createBranch(branchName);
+            client.updateFile(fileName, json.toString(), "update message", branchName);
+            client.createPullRequest("title", "message", branchName);
+
+            response.status(200);
+            return "OK";
+        });
+
     }
 }
